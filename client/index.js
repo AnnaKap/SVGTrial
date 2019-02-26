d5bae75b2b801e99a96460fcbbdc620dc58f8337
Anna Kaplan <annakaplanart@gmail.com>
2019-02-26 13:58:31 -0500

    smiley face on hover art

--- client/index.js
+++ client/index.js
@@ -2,8 +2,14 @@
 import SVG from 'svg.js'
 
 if (SVG.supported) {
-  var draw = SVG('drawing')
-  var rect = draw.rect(100, 100)
+  const smileDraw = SVG('smile').size(300,300)
+  const smile = smileDraw.path('M 10 60 q 55 0 110 0')
+  smile.fill('none')
+  smile.stroke({ color: '#f06', width: 4, linecap: 'round', linejoin: 'round' })
+  // var ellipse = smileDraw.ellipse(150, 100).fill('#f06').move(20, 20)
+  const eye1 = smileDraw.ellipse(20, 5).fill('#f06').move(10,10)
+  const eye2 = smileDraw.ellipse(20, 5).fill('#f06').move(100,10)
+// path.animate(2000).plot('M 10 10 q 55 200 110 0').loop(true, true)
 } else {
   alert('SVG not supported')
 }
